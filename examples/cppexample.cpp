6b011401d2374210dc8dbc032ea35f9f26ccab51
Ilya Prusskikh <prusskih.ilay@gmail.com>
2023-02-20 18:32:26 +0300

    Initial commit

--- examples/cppexample.cpp
+++ examples/cppexample.cpp
@@ -5,7 +5,7 @@
 #include "clhash.h"
 
 int main(void) {
-    clhash::CLHash h(UINT64_C(0x23a23cf5033c3c81), UINT64_C(0xb3816f6a2c68e530));
+    util::CLHash h(UINT64_C(0x23a23cf5033c3c81), UINT64_C(0xb3816f6a2c68e530));
     std::vector<int> vec{1, 3, 4, 5, 2, 24343};
     uint64_t vechash = h(vec);
 
